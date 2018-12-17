02d7ceeca9c73115c7ff1edcbdbf6b4b1702976b
Wihogora Wivine <wwihogora@gmail.com>
2018-12-17 10:12:28 +0200

    Add variables

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function getResult(){
-    var side1=parseFloat(document.getElementById('side1').value);
-    var side2=parseFloat(document.getElementById('side2').value);
-    var side3=parseFloat(document.getElementById('side3').value);
+    var side1=parseFloat(document.getElementById("side1").value);
+    var side2=parseFloat(document.getElementById("side2").value);
+    var side3=parseFloat(document.getElementById("side3").value);
 }
