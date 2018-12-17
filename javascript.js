c2848890cf940c77f04f1f377264909bd73a3df4
Wihogora Wivine <wwihogora@gmail.com>
2018-12-17 10:08:20 +0200

    Add variables

--- javascript.js
+++ javascript.js
@@ -1,29 +1,5 @@
 function getResult(){
-var hypotenuse= parseFloat(prompt("side hypotenuse"));
-var opposite = parseFloat(prompt("side opposite"));
-var adjacent = parseFloat(prompt("side adjacent"));
-/* var determine = function(hypotenuse,opposite, adjacent){
-if (hypotenuse === opposite && opposite === adjacent && hypotenuse === adjacent){
-alert("Equilateral");
-}
-
-else if ((hypotenuse === opposite && opposite === adjacent) (hypotenuse != adjacent && opposite !== adjacent));
-alert("Isosceles");
-
-}
-determine(); */
-
-
-if(hypotenuse === opposite && hypotenuse === hypotenuse && opposite === adjacent){
-alert("Equilateral");
-}
-else if(hypotenuse === opposite || hypotenuse === adjacent || opposite === adjacent){
-alert("Isosceles");
-}
-else if((hypotenuse + opposite) <= adjacent || (hypotenuse + adjacent) <= opposite || (opposite + adjacent) <= hypotenuse) {
-alert("Not a definite triangle");
-}
-else{
-alert("Scalene");
-}
+    var side1=parseFloat(document.getElementById('side1').value);
+    var side2=parseFloat(document.getElementById('side2').value);
+    var side3=parseFloat(document.getElementById('side3').value);
 }
